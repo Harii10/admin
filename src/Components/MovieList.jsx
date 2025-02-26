8685f6b48a944605f26f87871ed9159ebc11ba74
Harii10 <hariharachandru07@gmail.com>
2025-02-26 08:40:48 +0530

    added a delete button

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -16,6 +16,28 @@ function MovieList() {
         setMessage("❌Error Fetching.");
       });
   }, []);
+
+  const deleteMovie = async (movieId) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/delete-movie/${movieId}/`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      const result = await response.json();
+      console.log(result);
+  
+      if (response.ok) {
+        // ✅ Corrected: Update `movie` state instead of `movieInfo`
+        setMovie((prevMovies) => prevMovies.filter((movie) => movie.id !== movieId));
+      } else {
+        console.error("Error deleting movie:", result.error);
+      }
+    } catch (error) {
+      console.error("Error deleting artist:", error);
+    }
+  };
+
   return (
     <>
       <div className=" grid grid-cols-4 gap-6 h-screen">
@@ -31,7 +53,7 @@ function MovieList() {
               <div className="mt-4 sm:flex sm:gap-4">
                 <strong className="font-medium">{res.Movie_Name}</strong>
                 <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
+                <button className="rounded-sm bg-red-500 text-white p-3" onClick={() => deleteMovie(res.id)}>Delete</button>
               </div>
             </div>
           ))}
